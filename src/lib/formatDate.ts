48e71b829a40e6bfbb6fc7df7376c9fce7ecd1c6
Romeo <45272844+Riki1200@users.noreply.github.com>
2023-02-25 18:53:28 -0400

    feat: change language blog

--- src/lib/formatDate.ts
+++ src/lib/formatDate.ts
@@ -1,4 +1,8 @@
 const formatDate = (date: string, locale = 'zh-TW') => {
+  //locales from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
+
+  //get current locale from browser
+
   const now = new Date(date).toLocaleDateString(locale, {
     year: 'numeric',
     month: 'long',
